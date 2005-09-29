8e30c7e1c0844424f347967434ad388321c49317
pavelsher <pavelsher@840f080f-be6c-4074-b703-9857d52999f0>
2005-09-29 06:48:29 +0000

    Errors processing in the builder corrected

--- src/org/tigris/juxy/builder/BuilderErrorListener.java
+++ src/org/tigris/juxy/builder/BuilderErrorListener.java
@@ -7,7 +7,7 @@ import javax.xml.transform.ErrorListener;
 import javax.xml.transform.TransformerException;
 
 /**
- * $Id: BuilderErrorListener.java,v 1.2 2005-08-05 08:38:29 pavelsher Exp $
+ * $Id: BuilderErrorListener.java,v 1.3 2005-09-29 06:48:29 pavelsher Exp $
  * <p/>
  * @author Pavel Sher
  */
@@ -15,14 +15,14 @@ public class BuilderErrorListener implements ErrorListener {
     private static Log logger = LogFactory.getLog(BuilderErrorListener.class);
 
     public void error(TransformerException exception) throws TransformerException {
-        logger.error(exception);
+        logger.error(exception.getMessageAndLocation());
     }
 
     public void fatalError(TransformerException exception) throws TransformerException {
-        logger.fatal(exception);
+        logger.fatal(exception.getMessageAndLocation());
     }
 
     public void warning(TransformerException exception) throws TransformerException {
-        logger.warn(exception);
+        logger.warn(exception.getMessageAndLocation());
     }
 }
